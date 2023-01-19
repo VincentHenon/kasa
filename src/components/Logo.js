9c672992eb29ed7a7b5065e73acfbf098da32c7b
Vincent Henon <mr.henonvincent@gmail.com>
2023-01-19 16:31:59 +0100

    deploy to gh-pages

--- src/components/Logo.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Logo from "../assets/images/Header/Logo.png";
-
-const logo = (props) => (
-    <img src={Logo} alt="logo Kasa" className="logo" />
-);
-
-export default logo;
--- src/components/Tags.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Tags = ({ tag }) => (
-    <div className="tags" >
-        <p>{tag}</p>
-    </div >
-);
-
-export default Tags;
